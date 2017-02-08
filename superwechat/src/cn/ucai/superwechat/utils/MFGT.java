184284b323b8dd5adffba9f13b9364346cc14f74
xiongqian <736182683@qq.com>
2017-02-08 11:04:14 +0800

    添加欢迎页面和相关工具类

---
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,37 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
+
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+public class MFGT {
+    public static void startActivity(Activity activity, Class<?> clz) {
+        activity.startActivity(new Intent(activity,clz));
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void startActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+
+    public static void gotoLogin(Activity activity) {
+        startActivity(activity, LoginActivity.class);
+    }
+    public static void gotoRegister(Activity activity) {
+        startActivity(activity, RegisterActivity.class);
+    }
+
+}
