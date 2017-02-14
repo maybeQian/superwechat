fcd5f68eec9276df2baa4cba7de6facdaf17db7f
xiongqian <736182683@qq.com>
2017-02-14 13:11:19 +0800

    发送添加好友的通知

--- superwechat/src/cn/ucai/superwechat/utils/MFGT.java
+++ superwechat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -7,9 +7,12 @@ import android.support.v4.app.FragmentActivity;
 
 import com.hyphenate.easeui.domain.User;
 
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.net.OnCompleteListener;
 import cn.ucai.superwechat.ui.AddContactActivity;
+import cn.ucai.superwechat.ui.ApplyActivity;
+import cn.ucai.superwechat.ui.FriendActivity;
 import cn.ucai.superwechat.ui.LoginActivity;
 import cn.ucai.superwechat.ui.MainActivity;
 import cn.ucai.superwechat.ui.RegisterActivity;
@@ -60,6 +63,12 @@ public class MFGT {
     }
 
     public static void gotoFriend(Activity activity, User user) {
+        Intent intent = new Intent(activity, FriendActivity.class);
+        intent.putExtra("user", user);
+        startActivity(activity,intent);
+    }
 
+    public static void gotoApply(Activity activity, String userName) {
+        startActivity(activity,new Intent(activity, ApplyActivity.class).putExtra(I.User.USER_NAME,userName));
     }
 }
